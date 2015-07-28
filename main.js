a52f919fa8d8a0b24849acf95ce3f81d33b141bd
Lynn Samuelson <lynn.samuelson@gmail.com>
2015-07-28 14:46:52 -0500

    completed quiz

--- main.js
+++ main.js
@@ -4,11 +4,8 @@ $(document).ready(function() {
 
   // Write a JavaScript function that accepts an array as an argument, and
   // returns a new array that is sorted.
-  var number = [21, 33, 5, 9, 99, 34];
-  number.sort(function(a,b) {
-  return a - b;
-  });
-  console.log(number);
+  var number = [];
+  
 
   // Write a JavaScript function that accepts an array, and returns a new
   // array that contains elements in the array you passed in that are 
@@ -20,7 +17,6 @@ $(document).ready(function() {
 
   var bigger = number.filter(larger);
 
-  console.log(bigger);
 
   // var button = document.getElementById("math");
   // var output = document.getElementById("output");
@@ -34,9 +30,33 @@ $(document).ready(function() {
    
   // }
   var input;
-  function show() {
+  $('button').click(function() {
     input = $('#inputNum').val();
     console.log(input);
+    number = input.split(",")
+      console.log(number);
+
+  var bigger = number.filter(larger);
+  console.log("numbers larger than 25 are " + bigger);    
+  
+  bigger.sort(function(a,b) {
+  return a - b;
+  });
+  console.log("in order the numbers are " + bigger);  
+    
+  
+  
+
+  for (var i = 0; i < bigger.length; i++) {
+
+    el.innerHTML += "<div class="number">" + bigger[i] + "</div>";
+    console.log("the numbers are " + output);
   }
 
+  })
+  
+// var inputArray = (input.split(',') != null);
+// console.log(inputArray);  
+
 })
+
